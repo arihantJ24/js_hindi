dcf38a0357563df43c4f5e2921338f2781aa04b8
Arihant Jain <111786428+arihantJ24@users.noreply.github.com>
2024-05-19 07:33:25 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2<1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// in Js comparison operator < > <= >= and equality ==  both work differently 
+// comparison convert the null as 0 that's why line 8 say true but 6 say false 
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+
+// === strictly check
+
+console.log("2" === 2);
