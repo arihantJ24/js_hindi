57d18b3f486cc2efe8b6c7baa4bbb623dce0b7d3
Arihant Jain <111786428+arihantJ24@users.noreply.github.com>
2024-05-19 06:57:26 +0000

    day1

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 5;
+let accountEmail = "jainr@gmail.com";
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+// accountId = 2;
+console.log(accountId);
+
+accountEmail= "34@gmail.com"
+accountPassword = "56";
+accountCity = "delhi"
+
+/*  */
+console.table([accountId,accountPassword,accountEmail,accountCity , accountState]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all code as a new version.
+
+// alert("3+3"); // we are using node.js not browser 
+
+// code readability show be high
+
+//datatypes
+
+let name = "ari" // string
+let age = 18 // number
+let islogin = false // boolean
+
+
+// number -> 2 to power 52
+// bigint
+// string -> ""
+// boolean
+// null -> standalone value , "typeof(null) -> object"
+// undefined "typeof(null) -> undefined"
+// symbol -> unique
+
+
+// object 
+
+console.log(typeof(undefined));
