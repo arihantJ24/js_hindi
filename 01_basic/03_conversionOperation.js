14d20094c9aaff0e6a8b8accd85cf4461cf1a5cc
Arihant Jain <111786428+arihantJ24@users.noreply.github.com>
2024-05-19 07:24:39 +0000

    lect6

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,1 +1,56 @@
-let score = 33;
+// let score = "ari";
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1;
+
+let isLoggedIn = "ari";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 -> true
+// 0 -> false
+// ""-> false
+// "ari" -> true
+
+// let a = 33;
+// let stringNumber = String(a);
+// console.log(typeof stringNumber)
+// console.log(stringNumber);
+
+// **************** Operations **********************
+
+// let value = 3;
+// let netValue = -value;
+// console.log(netValue);
+
+
+// console.log(4*3);
+// console.log(4**3);
+
+// let string1 = "ari";
+// let string2 = "jain";
+
+// let str3 = string1+string2;
+// console.log(str3);
+
+
+// console.log("2" + 3);
+// console.log(1 +"3");
+// console.log(1 +"3" + "3");
+// console.log(1 +3 + "3");
+// console.log("1" +3 + "3");
+
+// console.log(+true);
+
+// let num1,num2,num3;
+// num1 = num2 = num3 = 3;
+
+let gameCounter = 
